a214ee9fd05fd36d7d0c0ffb77c2e66cacb3f3a6
Eric Chang <fsc8504@gmail.com>
2023-02-18 21:00:23 -0800

    add hover effect with trail

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const gridContainer = document.querySelector('#container');
 const gridSize = 16;
+let penColor = 'red';
 
 // set the grid contatiner to display as 16 x 16
 gridContainer.getElementsByClassName.display = 'grid';
@@ -8,9 +9,19 @@ gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
 
 for (let i = 1; i <= gridSize * gridSize; i++) {
-    const content = document.createElement('div');
-    content.classList.add('grid-item');
-    console.log('hahahaha')
-    gridContainer.appendChild(content);
+  const content = document.createElement('div');
+  content.classList.add('grid-item');
+  gridContainer.appendChild(content);
+
+  // Add a "pen" effect to each grid item using event listeners
+  content.addEventListener('mouseenter', () => {
+    content.style.backgroundColor = penColor;
+  });
 }
 
+// Add a color picker to change the pen color
+const colorPicker = document.querySelector('.color-picker');
+
+colorPicker.addEventListener('change', () => {
+  penColor = colorPicker.value;
+});
